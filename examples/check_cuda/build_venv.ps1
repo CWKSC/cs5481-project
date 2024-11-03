4b79da52dc17e1f72b795d6ab99f78722391421c
CWKSC <53114952+CWKSC@users.noreply.github.com>
2024-11-04 02:25:34 +0800

    Create check CUDA example

---
+++ examples/check_cuda/build_venv.ps1
@@ -0,0 +1,9 @@
+python -m venv .venv
+
+.venv\Scripts\activate
+
+python -m pip install --upgrade pip
+
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+
+
