9f5de35cc7a4c51b48ded3021ca0b49dd15dc6ba
CWKSC <53114952+CWKSC@users.noreply.github.com>
2024-11-05 21:46:52 +0800

    Bin to json

---
+++ stage/stage_2_data_processing/bin_to_json/main.py
@@ -0,0 +1,23 @@
+from util.core.data_storage import read_data_grid
+import pathlib
+import json
+
+current_folder = pathlib.Path(__file__).parent
+root_folder = current_folder.parent.parent.parent
+resources_folder = root_folder / "resources"
+
+bin_file_path = resources_folder / "bin" / "9gag-memes-llama-description-7k.bin"
+json_file_path = resources_folder / "json" / "text_description_llama_3_2_vision.json"
+
+df = read_data_grid(bin_file_path)
+post0 = df.posts[0]
+print(post0.id)
+print(post0.content)
+
+result = {}
+for post in df.posts:
+    result[post.id] = post.content
+
+with open(json_file_path, "w") as file:
+    json.dump(result, file, indent=4)
+
