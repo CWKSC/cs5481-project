a732b735fab6afc8cbe70e801489519b455278a0
CWKSC <53114952+CWKSC@users.noreply.github.com>
2024-11-05 19:05:50 +0800

    Update example, category by InternVL 2.0

--- examples/category_intern_vl_2/main.py
+++ examples/category_intern_vl_2/main.py
@@ -2,14 +2,20 @@ from lmdeploy import pipeline, TurbomindEngineConfig
 from lmdeploy.vl import load_image
 import pathlib
 import time
+import os
+
+# Set GPU: 0 / 1 / 2 / 3
+# nvidia-smi to check GPU usage
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 
 current_folder = pathlib.Path(__file__).parent
+output_folder = current_folder / "text_category_intern_vl_2"
 images_folder = current_folder / "images"
+
+prompt = """Tag the image with categories, tag can be more than one, not too many, separated by commas"""
 
 model = "OpenGVLab/InternVL2-8B"
 pipe = pipeline(model, backend_config=TurbomindEngineConfig(session_len=8192))
-
-prompt = """Tag the image with categories, tag can be more than one, not too many, separated by commas"""
 
 start_time = time.time()
 for image_path in images_folder.iterdir():
@@ -17,7 +23,7 @@ for image_path in images_folder.iterdir():
     image = load_image(path)
     response = pipe((prompt, image))
     print(response.text)
-    with open(f"category_intern_vl_2/{image_path.stem}.txt", "w") as f:
+    with open(f"{output_folder}/{image_path.stem}.txt", "w") as f:
         f.write(response.text)
 
 end_time = time.time()
