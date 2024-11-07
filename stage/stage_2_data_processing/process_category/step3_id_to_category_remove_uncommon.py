70bfe0cbb919254385acf830554f6ed28b9dbc6a
CWKSC <53114952+CWKSC@users.noreply.github.com>
2024-11-08 05:00:22 +0800

    Update stage 2, process category

--- stage/stage_2_data_processing/process_category/step3_id_to_category_remove_uncommon.py
+++ stage/stage_2_data_processing/process_category/step3_id_to_category_remove_uncommon.py
@@ -1,6 +1,6 @@
+from tqdm import tqdm
 import json
 import pathlib
-from tqdm import tqdm
 
 current_folder = pathlib.Path(__file__).parent.resolve()
 root_folder = current_folder.parent.parent.parent
@@ -11,11 +11,14 @@ id_to_category_list_json = category_folder / "step1_id_to_category_list.json"
 category_to_id_list_json = category_folder / "step2_category_to_id_list.json"
 
 # Output
-output_json = category_folder / "step3_id_to_category_remove_uncommon.json"
+id_to_category_remove_uncommon_json = (
+    category_folder / "step3_id_to_category_remove_uncommon.json"
+)
+id_to_category_top_30_json = category_folder / "step3_id_to_category_top_30.json"
 
+# Load data
 with open(category_to_id_list_json, "r") as file:
     category_to_id_list = json.load(file)
-
 with open(id_to_category_list_json, "r") as file:
     id_to_category_list = json.load(file)
 
@@ -26,8 +29,39 @@ for category, ids in category_to_id_list.items():
 
 # Remove uncommon categories (contain less than 3 ids)
 result = {}
-for id, categories in tqdm(id_to_category_list.items(), total=len(id_to_category_list)):
-    result[id] = list(filter(lambda category: category_to_num[category] > 3, categories))
+for id, categories in tqdm(
+    id_to_category_list.items(),
+    total=len(id_to_category_list),
+    desc="Remove uncommon categories",
+):
+    result[id] = list(
+        filter(lambda category: category_to_num[category] > 3, categories)
+    )
 
-with open(output_json, "w") as file:
+# Save result
+with open(id_to_category_remove_uncommon_json, "w") as file:
+    json.dump(result, file, indent=4)
+
+# Get top n categories
+top_n = 30
+top_n_categories = dict(
+    sorted(category_to_num.items(), key=lambda item: item[1], reverse=True)[
+        2 : 2 + top_n
+    ]
+)
+
+result = {}
+for id, categories in tqdm(
+    id_to_category_list.items(),
+    total=len(id_to_category_list),
+    desc=f"Remove categories not in top {top_n}",
+):
+    result_list = list(
+        filter(lambda category: category in top_n_categories, categories)
+    )
+    if len(result_list) > 0:
+        result[id] = result_list
+
+# Save result
+with open(id_to_category_top_30_json, "w") as file:
     json.dump(result, file, indent=4)
--- stage/stage_2_data_processing/process_category/step4_id_to_similar_id.py
+++ stage/stage_2_data_processing/process_category/step4_id_to_similar_id.py
@@ -1,36 +1,112 @@
-import json
-import pathlib
 from tqdm import tqdm
+import json
+import math
+import pathlib
 
 current_folder = pathlib.Path(__file__).parent.resolve()
 root_folder = current_folder.parent.parent.parent
 category_folder = root_folder / "resources" / "json" / "category"
 
 # Input
+category_to_id_list_json = category_folder / "step2_category_to_id_list.json"
 id_to_category_list_json = category_folder / "step3_id_to_category_remove_uncommon.json"
+id_to_category_list_top_30_json = category_folder / "step3_id_to_category_top_30.json"
 
 # Output
-output_json = category_folder / "step4_id_to_similar_id.json"
+id_to_similar_id_json = category_folder / "step4_id_to_similar_id.json"
+id_to_similar_id_from_top_30_json = (
+    category_folder / "step4_id_to_similar_id_from_top_30.json"
+)
 
+# Load data
+with open(category_to_id_list_json, "r") as file:
+    category_to_id_list = json.load(file)
 with open(id_to_category_list_json, "r") as file:
     id_to_category_list = json.load(file)
+with open(id_to_category_list_top_30_json, "r") as file:
+    id_to_category_list_top_30 = json.load(file)
+
+# Get IDF of each category
+total_ids = len(id_to_category_list.keys())
+print("Total ids:", total_ids)
+idf = {}
+for category, ids in category_to_id_list.items():
+    idf[category] = math.log((total_ids + 1) / (len(ids) + 1), total_ids + 1)
+
+# Sort idf by value
+idf = dict(sorted(idf.items(), key=lambda item: item[1], reverse=True))
+print(
+    "First 5 idf:",
+    ", ".join(
+        [f"{category}: {value:.3f}" for category, value in list(idf.items())[:5]]
+    ),
+)
+print(
+    "Last 5 idf:",
+    ", ".join(
+        [f"{category}: {value:.3f}" for category, value in list(idf.items())[-5:]]
+    ),
+)
 
 result = {}
-for id, categories in tqdm(id_to_category_list.items(), total=len(id_to_category_list), desc="Get similarity of each id"):
+for id, categories in tqdm(
+    id_to_category_list.items(),
+    total=len(id_to_category_list),
+    desc="Get similarity of each id with idf",
+):
     similarity = {}
     for other_id, other_categories in id_to_category_list.items():
         if id == other_id:
             continue
 
         # Get the number of categories that are the same between two ids
-        similarity_value = len(set(categories) & set(other_categories))
-        if similarity_value == 0:
+        intersection = set(categories) & set(other_categories)
+        if len(intersection) == 0:
             continue
+
+        # Calculate similarity value
+        similarity_value = sum(idf[category] for category in intersection)
+
         similarity[other_id] = similarity_value
-    
+
     # Keep only top 10 similar ids
-    similarity = dict(sorted(similarity.items(), key=lambda item: item[1], reverse=True)[:10])
+    similarity = dict(
+        sorted(similarity.items(), key=lambda item: item[1], reverse=True)[:10]
+    )
     result[id] = similarity
 
-with open(output_json, "w") as file:
+# Save result
+with open(id_to_similar_id_json, "w") as file:
+    json.dump(result, file, indent=4)
+
+
+result = {}
+for id, categories in tqdm(
+    id_to_category_list_top_30.items(),
+    total=len(id_to_category_list_top_30),
+    desc="Get similarity of each id from top 30 categories",
+):
+    similarity = {}
+    for other_id, other_categories in id_to_category_list_top_30.items():
+        if id == other_id:
+            continue
+
+        # Get the number of categories that are the same between two ids
+        intersection = set(categories) & set(other_categories)
+        if len(intersection) == 0:
+            continue
+
+        # Calculate similarity value
+        similarity_value = sum(idf[category] for category in intersection)
+
+        similarity[other_id] = similarity_value
+
+    # Keep only top n similar ids
+    similarity = dict(
+        sorted(similarity.items(), key=lambda item: item[1], reverse=True)[:1400]
+    )
+    result[id] = similarity
+
+# Save result
+with open(id_to_similar_id_from_top_30_json, "w") as file:
     json.dump(result, file, indent=4)
