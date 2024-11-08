d45f7757f6532452ceb95c5efb07db4cfa5ac9ba
CWKSC <53114952+CWKSC@users.noreply.github.com>
2024-11-09 00:18:44 +0800

    Update stage 3, word_cloud for meaning

---
+++ stage/stage_3_data_analysis/word_cloud/meaning.py
@@ -0,0 +1,66 @@
+from nltk.corpus import stopwords, words
+from wordcloud import WordCloud
+import json
+import pathlib
+import tqdm
+import numpy as np
+from PIL import Image
+import cv2
+import matplotlib.pyplot as plt
+from wordcloud import ImageColorGenerator
+
+current_folder = pathlib.Path(__file__).parent
+root_folder = current_folder.parent.parent.parent
+meaning_folder = root_folder / "resources" / "json" / "meaning"
+mask_folder = current_folder / "mask"
+results_folder = current_folder / "results"
+
+# Input
+file_path = meaning_folder / "id_to_meaning_list_tf_idf.json"
+mask_name = "doge"
+mask_extension = "png"
+mask_file_path = mask_folder / f"{mask_name}.{mask_extension}"
+
+# Output
+output_file_path = results_folder / "meaning_wordcloud.png"
+output_file_color_path = results_folder / "meaning_wordcloud_color.png"
+
+mask_color = np.array(Image.open(mask_file_path))
+
+# Transform mask to black and white
+mask_bw = cv2.cvtColor(mask_color, cv2.COLOR_BGR2GRAY)
+
+# Filter to black and white
+# mask_bw = np.where(mask_bw == 0, 255, 0)
+mask_bw = np.where(mask_bw > 230, 255, 0)
+
+# Save mask
+cv2.imwrite(mask_folder / f"{mask_name}_bw.png", mask_bw)
+
+with open(file_path, "r", encoding="utf-8") as file:
+    id_to_meaning_list_tf_idf = json.load(file)
+
+# Join all the tokens
+values = list(id_to_meaning_list_tf_idf.values())
+text = " ".join(" ".join(token_list) for token_list in values)
+
+# Color map
+# https://blog.csdn.net/geekqian/article/details/122599683
+
+wordcloud = WordCloud(
+    background_color="white",
+    # colormap="RdYlGn",
+    # mode="RGBA",
+    mask=mask_bw,
+).generate(text)
+wordcloud.to_file(output_file_path)
+
+image_colors = ImageColorGenerator(mask_color)
+plt.figure(figsize=[50, 50])
+plt.imshow(wordcloud.recolor(color_func=image_colors), interpolation="bilinear")
+plt.axis("off")
+
+# store to file
+plt.savefig(output_file_color_path, format="png")
+
+# plt.show()
